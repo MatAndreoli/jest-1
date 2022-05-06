a6dc83856ce447cfbe11ecf3380ed4e35099224a
MatAndreoli <andreoli.2003@gmail.com>
2022-05-06 14:38:30 -0300

    Update iterate.spec.js test

--- spec/iterate.spec.js
+++ spec/iterate.spec.js
@@ -2,28 +2,35 @@ describe('Given concat method', () => {
   describe('When it is called', () => {
     const tests = [
       {
-        expectIt: [].concat(['first', 'second']).concat(['last']),
-        toEqual: ['first', 'second', 'last'],
+        input: [].concat(['first', 'second']).concat(['last']),
+        expected: ['first', 'second', 'last'],
       },
       {
-        expectIt: [].concat([1, 2]).concat([3]),
-        toEqual: [1, 2, 3],
+        input: [].concat([1, 2]).concat([3]),
+        expected: [1, 2, 3],
       },
       {
-        expectIt: [].concat(['one', 'two']).concat(['three']),
-        toEqual: ['one', 'two', 'three'],
+        input: [].concat(['one', 'two']).concat(['three']),
+        expected: ['one', 'two', 'three'],
       },
       {
-        expectIt: [].concat([2, 3]).concat([5]),
-        toEqual: [2, 3, 5],
+        input: [].concat([2, 3]).concat([5]),
+        expected: [2, 3, 5],
       },
       {
-        expectIt: [].concat([1, 1]).concat([2]),
-        toEqual: [1, 1, 2],
+        input: [].concat([1, 1]).concat([2]),
+        expected: [1, 1, 2],
       },
     ];
-    it.each(tests)('Then should concat the given values', (expectObj) => {
-      expect(expectObj.expectIt).toEqual(expectObj.toEqual)
+
+    //! it.each(tests)('Then should concat the given values %s - %s', (expectObj) => {
+    //!   expect(expectObj.input).toEqual(expectObj.expected)
+    //! });
+
+    tests.forEach((test) => {
+      it(`Then should concat the given values: ${test.input} ==> ${test.expected}`, () => {
+        expect(test.input).toEqual(test.expected);
+      });
     });
   });
 });
