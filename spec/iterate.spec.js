996a760b2b216d6affaede4cd941b51cfd378fcd
MatAndreoli <andreoli.2003@gmail.com>
2022-05-06 13:37:33 -0300

    Update iterate.spec.js test

--- spec/iterate.spec.js
+++ spec/iterate.spec.js
@@ -1,24 +1,29 @@
-
 describe('Given concat method', () => {
-    describe('When it is called', () => {
-        it('Then ', () => {
-            expect([].concat(['first', 'second']).concat(['last'])).toEqual(['first', 'second', 'last']);
-        // });
-
-        // it('Then ', () => {
-            expect([].concat([1, 2]).concat([3])).toEqual([1, 2, 3]);
-        // });
-
-        // it('Then ', () => {
-            expect([].concat(['one', 'two']).concat(['three'])).toEqual(['one', 'two', 'three']);
-        // });
-
-        // it('Then ', () => {
-            expect([].concat([2, 3]).concat([5])).toEqual([2, 3, 5]);
-        // });
-
-        // it('Then ', () => {
-            expect([].concat([1, 1]).concat([2])).toEqual([1, 1, 2])
-        });
+  describe('When it is called', () => {
+    const tests = [
+      {
+        expectIt: [].concat(['first', 'second']).concat(['last']),
+        toEqual: ['first', 'second', 'last'],
+      },
+      {
+        expectIt: [].concat([1, 2]).concat([3]),
+        toEqual: [1, 2, 3],
+      },
+      {
+        expectIt: [].concat(['one', 'two']).concat(['three']),
+        toEqual: ['one', 'two', 'three'],
+      },
+      {
+        expectIt: [].concat([2, 3]).concat([5]),
+        toEqual: [2, 3, 5],
+      },
+      {
+        expectIt: [].concat([1, 1]).concat([2]),
+        toEqual: [1, 1, 2],
+      },
+    ];
+    it.each(tests)('Then should concat the given values', (expectObj) => {
+      expect(expectObj.expectIt).toEqual(expectObj.toEqual)
     });
+  });
 });
